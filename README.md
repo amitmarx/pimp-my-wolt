9fe2dde6f73dc86b91db5acb41e0a9450fc0350e
Amit Marx <amitm@nonamesecurity.com>
2022-08-28 00:16:12 +0300

    allow to auto split payment

--- README.md
+++ README.md
@@ -1,88 +1,20 @@
-# Readme
+# Background
 
-# What is this project about
-
-My team uses Wolt everyday for our lunch order.  
+My team uses Wolt every day for our lunch order.  
 We use Wolt's shared order and split the payment on Cibus.  
-After few weeks of this practice, I thought it might be cool if I could invite all of my team at once   
-rather than the "one by one" option currently available on Wolt.  
-Then I've noticed a bigger pain for my team which is the manual payment split,  
-so I decided to create this Chrome extension to solve these everyday hassles.
+Instead of manually splitting the payment on Cibus,  
+I've created this nice Chrome extension to achieve it automatically. 
 
-# Features
+# Installation
 
-## Invite all group members
+1. Visit [Pimp my Wolt](https://chrome.google.com/webstore/detail/pimp-my-wolt/edfemdoibbcbmkojfdeldnllcbnpmfld) Chrome Web Store page and click `Add to Chrome`.  
 
-![PR/gifs/invite-all.gif](PR/gifs/invite-all.gif)
+2. Set your group name. Different users may share the same group name for easier maintenance of group members:  
 
-## Split shared order payment
+https://user-images.githubusercontent.com/22027545/188240633-95b28090-7746-4547-a123-ffe967babd29.mov
+   
+3. Onboard new members to your group. Onboarding is done once and used for every future order.  
+If you missed someone, don't worry, you can always try our automatic splitting algorithm.  
+From now on you'll never split payments manually! 🎉
 
-![PR/gifs/split-payment.gif](PR/gifs/split-payment.gif)
-
-# Installation Guide
-
-1. Browse to the extension page on [Google Web Store](https://chrome.google.com/webstore/detail/pimp-my-wolt/edfemdoibbcbmkojfdeldnllcbnpmfld) and click `Add to Chrome`.
-2. Set your group name on the extension's options.  
-In order to enter to the options page, browse to:  
-chrome://extensions/?options=edfemdoibbcbmkojfdeldnllcbnpmfld  
-For more details about groups, [please see here](#What-is-Group-and-What-is-it-used-for).
-3. [Make sure](#List-group-members) your group is properly configured. 
-
-# What is Group and What is it used for
-
-Group is a set of people you typically share Wolt's orders with.  
-Each group member represented by an object containing its Wolt name and Cibus name.  
-Formally:
-
-```tsx
-type Group = GroupMember[]
-
-interface GroupMember {
-	woltName: String;
-	cibusName: String;
-}
-```
-
-This mapping between Wolt name and Cibus name allows the extension to remember who ordered what on Wolt, and then "charge" the right amount the right person on Cibus.  
-
-# Manage Group Members
-
-Group members are managed via a crud service.  
-In the following demonstration I'll use bash to interact with this service,  
-but you may chose your favourite alternative. 
-
-## Add new group member
-
-```bash
-curl -X POST \
-  https://amitmarx.wixsite.com/pimp-my-wolt/_functions/group_member/{GROUP_NAME} \
-  -H 'content-type: application/json' \
-  -d '{
-        "cibusName": "{CIBUS_NAME}",
-        "woltName": "{WOLT_NAME}"
-}'
-```
-
-- GROUP_NAME is your team's chosen name.  
-(Groups are created on the fly, first group member will initiate the group).
-- WOLT_NAME is your display name on Wolt.  
-It may be found here: [https://wolt.com/en/me](https://wolt.com/en/me).
-- CIBUS_NAME is your display name on Cibus.  
-It may be found here: [https://www.mysodexo.co.il/new_my/new_my_details.aspx](https://www.mysodexo.co.il/new_my/new_my_details.aspx).
-
-## Delete group member
-
-```bash
-curl -X DELETE \
-  https://amitmarx.wixsite.com/pimp-my-wolt/_functions/group_member/{MEMBER_ID}
-```
-
-- MEMBER_ID may be found from the [list group members](#list-group-members) endpoint.
-
-## List group members
-
-```bash
-curl https://amitmarx.wixsite.com/pimp-my-wolt/_functions/list_group_members/{GROUP_NAME}
-```
-
-- GROUP_NAME is your team's chosen name.
+https://user-images.githubusercontent.com/22027545/188240914-9a76a532-df71-4f45-a79a-55be08b3d91e.mov
--- src/pimp-my-cibus.js
+++ src/pimp-my-cibus.js
@@ -1,9 +1,17 @@
 (function () {
+  const logoUrl = chrome.runtime.getURL(
+      "assets/icons/pimp-my-wolt-icon-48.png"
+  );
   const { groupManager, biLogger } = window.pimpMyWolt;
   const allGuests = groupManager.getAllGuests();
 
   const paymentButtonSettings = {
     settledAttribute: "settled",
+  };
+  
+  const postPayment = {
+    divId: "postPaymentDiv-pimpMyWolt",
+    autoPaymentButtonId: "autoPaymentButton-pimpMyWolt"
   };
 
   const getPaymentButton = () => document.getElementById("pnlBtnPay");
@@ -28,6 +36,21 @@
       .iterateNext();
   }
 
+  async function getAutoMatch({woltNames, cibusNames}) {
+    const response = await fetch(
+      `https://amitmarx.wixsite.com/pimp-my-wolt/_functions/cibus_wolt_auto_matches`, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({woltNames, cibusNames})
+      }
+    );
+    const responseJson = await response.json();
+    return responseJson;
+  }
+
   async function handleCibusPayment() {
     const guests = await allGuests;
     chrome.storage.local.get(
@@ -50,6 +73,7 @@
           };
         });
         const settledGuests = await setGuestsDebts(guestDebts);
+        handlePostPayment({settledGuests, guestDebts})
         publishSplitPaymentEvent({
           restaurant,
           settledGuests,
@@ -60,13 +84,96 @@
     );
   }
 
+  async function autoMatchCibusNameToBet(debts){
+    const cibusNames = getAllCibusNames()
+    const woltNames = debts.map(({woltName})=> woltName)
+    const autoMapping = await getAutoMatch({cibusNames, woltNames})
+    const cibusToWolt = autoMapping.reduce((o, item) => {
+      o[item.woltName] = item.cibusName;
+      return o;
+    }, {});
+    return debts.map((d) => {
+      return {
+        ...d,
+        cibusName: cibusToWolt[d.woltName],
+      };
+    });
+  }
+
+  async function autoSplitDebt(asyncDebts) {
+    const debts = await asyncDebts;
+    const settledGuests = await setGuestsDebts(debts);
+    publishAutoSplitPaymentEvent({settledGuests, guestsOrders: debts})
+    const postPaymentDiv = document.querySelector(`#${postPayment.divId}`)
+    postPaymentDiv.innerHTML = "<span>מקווים שעזרנו... &#128521;</span>"
+  }
+  
+  function getPostPaymentContent({ settledGuests, guestDebts }) {
+    const leftToSplit = settledGuests.length < guestDebts.length - 1;
+    const div = document.createElement("div");
+    div.setAttribute("id", postPayment.divId);
+
+    const splitMessage =
+      settledGuests.length > 0
+        ? " הופה! הצלחנו לפצל " + settledGuests.length + "תשלומים עפ״י המיפוי בקבוצה שלך."
+        : `לא הצלחנו לפצל תשלומים עפ״י המיפוי בקבוצה.`;
+    const splitSpan = document.createElement("span")
+    splitSpan.appendChild(document.createTextNode(splitMessage));
+    div.appendChild(splitSpan);
+    div.appendChild(document.createElement("br"))
+
+    const settledMessage = leftToSplit
+      ? "אל דאגה, אפשר לנסות את מנגנון הפיצול האוטומטי שלנו"
+      : "";
+    const settledSpan = document.createElement("span");
+    settledSpan.appendChild(document.createTextNode(settledMessage))
+    div.appendChild(settledSpan);
+
+    if (leftToSplit) {
+      const btn = document.createElement("div");
+      btn.setAttribute("id", postPayment.autoPaymentButtonId);
+      btn.appendChild(document.createTextNode("פצל אוטומטית"));
+
+      const logoImage = document.createElement('img');
+      logoImage.src = logoUrl;
+      btn.appendChild(logoImage)
+
+      const settledNames = settledGuests.map(x=> x.name)
+      const debts = guestDebts.filter(({woltName}) => !settledNames.includes(woltName))
+      const debtsWithAutoMatch = autoMatchCibusNameToBet(debts)
+      btn.onclick = () => autoSplitDebt(debtsWithAutoMatch);
+      div.appendChild(btn);
+    }
+    return div;
+  }
+
+  function handlePostPayment({ settledGuests, guestDebts }) {
+    const content = getPostPaymentContent({ settledGuests, guestDebts });
+    const splitPanel = document.querySelector("#pnlSplitPay");
+    splitPanel.prepend(content);
+  }
+  
+  function getAllCibusNames() {
+    const notSelectedUsers = new Array(
+      ...document.querySelectorAll("label>input")
+    )
+      .map((x) => x?.parentNode)
+      .map((x) => x?.innerText);
+    const selectedUsers = new Array(
+      ...document.querySelectorAll("#splitList label")
+    )
+      .map((x) => x?.innerText)
+      .filter((x) => x && !x.includes("הוספת חברים"));
+    return [...notSelectedUsers, ...selectedUsers];
+  }
+
   async function publishSplitPaymentEvent({
     restaurant,
     settledGuests,
     guestsOrders,
     deliveryPrice,
   }) {
-    const allCibusUsersAvailable = new Array(...document.querySelectorAll('label>input')).map(x=>x?.parentNode).map(x=>x?.innerText);
+    const allCibusUsersAvailable = getAllCibusNames()
     const currentCibusUser = document.querySelector("#lblMyName").innerText;
     const currentUser =
       (await allGuests).find((guest) => guest.cibusName === currentCibusUser)
@@ -77,6 +184,23 @@
       settledGuests,
       guestsOrders,
       deliveryPrice,
+      allCibusUsersAvailable
+    });
+  }
+
+  async function publishAutoSplitPaymentEvent({
+    settledGuests,
+    guestsOrders,
+  }) {
+    const allCibusUsersAvailable = getAllCibusNames()
+    const currentCibusUser = document.querySelector("#lblMyName").innerText;
+    const currentUser =
+      (await allGuests).find((guest) => guest.cibusName === currentCibusUser)
+        ?.woltName || currentCibusUser;
+    biLogger.logEvent("auto_split_payment", {
+      userName: currentUser,
+      settledGuests,
+      guestsOrders,
       allCibusUsersAvailable
     });
   }
@@ -129,5 +253,5 @@
       openSplitPaymentTable();
       handleCibusPayment();
     }
-  }, 200);
+  }, 100);
 })();
